09eed2b70edbcf86d793ceccd64e685191a084a7
Yefer98 <yyefer98@gmail.com>
2024-06-11 21:18:50 -0500

    CircularProgress en DianComponent

--- src/components/DianComponent.jsx
+++ src/components/DianComponent.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../firebaseConfig';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 const DianComponent = () => {
   const [documents, setDocuments] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchDocuments = async () => {
@@ -11,20 +14,29 @@ const DianComponent = () => {
       let docs = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
       docs.sort((a, b) => a.orden - b.orden);
       setDocuments(docs);
+      setLoading(false);
     };
 
     fetchDocuments();
   }, []);
 
   return (
-      documents.map((doc) => (
-        <section key={doc.id} className="reseña" id={doc.dianId}>
-          <div className="title-reseña">
-            <h1>{doc.titulo}</h1>
-          </div>
-          <p>{doc.parrafo}</p>
-        </section>
-      ))
+    <div>
+      {loading ? (
+        <Box sx={{ height: '100vh', display: 'flex', justifyContent: 'center' }}>
+         <CircularProgress sx={{ color: '#2c1b4d' }} />
+        </Box>
+      ) : (
+        documents.map((doc) => (
+          <section key={doc.id} className="reseña" id={doc.dianId}>
+            <div className="title-reseña">
+              <h1>{doc.titulo}</h1>
+            </div>
+            <p>{doc.parrafo}</p>
+          </section>
+        ))
+      )}
+    </div>
   );
 };
 
